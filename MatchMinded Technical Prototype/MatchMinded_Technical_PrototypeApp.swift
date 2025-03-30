4752e7aae55c83f0d98138b46b48793f9df0e6dc
Carter <cartermanahan@gmail.com>
2025-03-30 18:54:52 +0100

    Initial Commit

---
+++ MatchMinded Technical Prototype/MatchMinded_Technical_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMinded_Technical_PrototypeApp.swift
+//  MatchMinded Technical Prototype
+//
+//  Created by Carter Manahan on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MatchMinded_Technical_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
